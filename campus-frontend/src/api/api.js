08960815c135ac077006b70da5dcf3aea8b05808
Diwakar  Nadar <nadardiwakar456@gmail.com>
2025-12-22 18:40:00 +0530

    Update api.js

--- campus-frontend/src/api/api.js
+++ campus-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/auth/",
+  baseURL: "https://campus-ride-6vle.onrender.com/auth/",
 });
 
 // 🔒 Request interceptor
